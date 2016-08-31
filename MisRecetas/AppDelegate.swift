3cb0be8739e8c8de920132536e49e880ebbac4c6
Juan Gabriel <juangabriel@frogames.es>
2016-08-31 10:26:54 +0200

    Hemos hecho privado el método didFinishLaunching del App Delegate por el cambio de iOS 10 beta 6.

--- MisRecetas/AppDelegate.swift
+++ MisRecetas/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         UINavigationBar.appearance().barTintColor = UIColor(red: 240.0/255.0, green: 113.0/255.0, blue: 115.0/255.0, alpha: 1.0)
